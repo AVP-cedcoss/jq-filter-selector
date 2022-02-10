58faa55557a72f36ddf0153e794379f7e8157938
Anugrah Vishwas Paul <anugrahvishwaspaul@cedcoss.com>
2022-02-10 13:15:23 +0530

    Everything Working AJAX, Search added

--- src/products.js
+++ src/products.js
@@ -1,82 +1,283 @@
-var products = [{
-					"id": "100",
-					"name": "iPhone 4S",
-					"brand": "Apple",
-					"os": "iOS"
-				},
-				{
-					"id": "101",
-					"name": "Moto X",
-					"brand": "Motorola",
-					"os": "Android"	
-				},
-				{
-					"id": "102",
-					"name": "iPhone 6",
-					"brand": "Apple",
-					"os": "iOS"
-				},
-				{
-					"id": "103",
-					"name": "Samsung Galaxy S",
-					"brand": "Samsung",
-					"os": "Android"
-				},
-				{
-					"id": "104",
-					"name": "Google Nexus",
-					"brand": "ASUS",
-					"os": "Android"
-				},
-				{
-					"id": "105",
-					"name": "Surface",
-					"brand": "Microsoft",
-					"os": "Windows"
-				}];
+//GLOBAL PRODUCT ARRAY
 
-$(document).ready(function()
+var products = [
+  {
+    id: "100",
+    name: "iPhone 4S",
+    brand: "Apple",
+    os: "iOS",
+  },
+  {
+    id: "101",
+    name: "Moto X",
+    brand: "Motorola",
+    os: "Android",
+  },
+  {
+    id: "102",
+    name: "iPhone 6",
+    brand: "Apple",
+    os: "iOS",
+  },
+  {
+    id: "103",
+    name: "Samsung Galaxy S",
+    brand: "Samsung",
+    os: "Android",
+  },
+  {
+    id: "104",
+    name: "Google Nexus",
+    brand: "ASUS",
+    os: "Android",
+  },
+  {
+    id: "105",
+    name: "Surface",
+    brand: "Microsoft",
+    os: "Windows",
+  },
+];
+
+//GLOBAL PRODUCT ARRAY
+
+//Complete Table Display without Filters on Load
+$(document).ready(function () 
 {
-	display();
+  displayFilters();
+  displayTable();
+});
+
+//To Add Content to HTML Document
+var html = "";
+
+let brandSelected = "";
+let osSelected = "";
+
+//Display Filters
+function displayFilters() 
+{
+  var osList = [];
+  var brandList = [];
+
+  for (var i = 0; i < products.length; i++) {
+    osList.push(products[i].os);
+    brandList.push(products[i].brand);
+  }
+
+  //Cleaning and selecting unique element for OS List
+  let os = osList.filter((item, i, ar) => ar.indexOf(item) === i);
+  osList = "";
+  for (var i = 0; i < os.length; i++)
+    osList +=
+      "<a href='#' data-os=" + os[i] + " class = 'os'>" + os[i] + "</a>";
+
+  //Cleaning and selecting unique element for Brand List
+  let brnd = brandList.filter((item, i, ar) => ar.indexOf(item) === i);
+  brandList = "";
+  for (var i = 0; i < brnd.length; i++)
+    brandList += "<a href='#' data-brand=" + brnd[i] + " class = 'brand'>" + brnd[i] + "</a>";
+
+  html =
+    "<div class='dropdown'>\
+		<button class='dropbtn' id='brandButton'>Brands</button>\
+			<div id='brandList' class='dropdown-content'>\
+				<input type='text' placeholder='Brands...' class='input' id='filterbrand'>" +
+    brandList +
+    "</div>\
+		<button class='dropbtn' id='osButton'>Operating System</button>\
+			<div id='osList' class='dropdown-content'>\
+				<input type='text' placeholder='OS...' class='input' id='filterOS'>" +
+    osList +
+    "</div>\
+	<button class='dropbtn' id='removeFilter'>Remove Filters</button>\
+			<div class='dropdown-content'>\"</div>\
+		</div>";
+}
+
+//Filter by Brand DropDown Button
+$(document).ready(function () 
+{
+	$(document).on("click", "#brandButton",function()
+	{
+		if($("#brandList").css("display")=="block"){
+			$("#brandList").css("display","none");
+		}
+		else{
+			$("#brandList").css("display","block");
+		}
+	});
+});
+
+//Filter by Operating System DropDown Button
+$(document).ready(function () 
+{
+	$(document).on("click", "#osButton",function()
+	{
+		if($("#osList").css("display")=="block"){
+			$("#osList").css("display","none");
+		}
+		else{
+			$("#osList").css("display","block");
+		}
+	});
+});
+
+//Remove Filters
+$(document).on("click", "#removeFilter", function () 
+{
+	brandSelected = "";
+	osSelected = "";
+	displayFilters();
+	displayTable();
+	$("#brandButton").html("Brands");
+	$("#osButton").html("Operating System");
 });
 
 //To display the Content of Product Array
-function display()
-{	
-	//Emptying the Division from any previous outputs
-	$("#footer").html("");
-
-	//Table Creation and Header
-	var html = "<table id='f-head'>\
+function displayTable() 
+{
+  //Table Creation and Header
+  html +=
+    "<table id='f-head'>\
 					<tr>\
 						<th>ID</th>\
 						<th>Name</th>\
 						<th>Brand</th>\
 						<th>Operating System</th>\
 						<th>Remove</th>\
-					</tr>";		
-	
+					</tr>\
+					<tbody id='tableBody'>";
+
 	//Traversing the Object Array and creating the rows
-	for (let i = 0; i < products.length; i++)
+	for (let i = 0; i < products.length; i++) 
 	{
-		html += "<tr class="+products[i].id+">\
-			<td>"+products[i].id+"</td>\
-			<td>"+products[i].name+"</td>\
-			<td>"+products[i].brand+"</td>\
-			<td>"+products[i].os+"</td>\
-			<td style='font-size:20px' class='fa'><a href='#' onclick='hideRow("+products[i].id+")'>&#xf00d;</a></td>\
-		</tr>";
+		if (osSelected != "" && brandSelected != "")
+		{
+			if (products[i].os == osSelected && products[i].brand == brandSelected)
+			{
+				tablePrint(i);
+			}
+		}
+
+		else if (osSelected != "")
+		{
+			if (products[i].os == osSelected)
+			{
+				tablePrint(i);
+			}
+		}
+
+		else if (brandSelected != "")
+		{
+			if (products[i].brand == brandSelected)
+			{
+				tablePrint(i);
+			}
+		}
+
+		else
+		{
+			tablePrint(i);
+		}
 	}
-	
+
 	//Closing the Table
-	html += "</table>"
-	
+	html += "</tbody></table>";
+
+	//Adding Search Bar
+	html += '<input id="search" class="input" type="text" placeholder="Search..." >';
+
 	//Adding Table to Divison for display
-	$("#footer").html(html);
+	$("#wrapper").html(html);
 }
 
-//To hide row on selection
-function hideRow(id)
+//Table Print
+function tablePrint(i)
 {
-	$("."+id+"."+id).hide();
+    html +=
+      "<tr class=" +
+      products[i].id +
+      ">\
+			<td>" +
+		products[i].id +
+		"</td>\
+			<td>" +
+		products[i].name +
+		"</td>\
+			<td>" +
+		products[i].brand +
+		"</td>\
+			<td>" +
+		products[i].os +
+		"</td>\
+			<td style='font-size:20px' class='fa'><a href='#' onclick='hideRow(" +
+		products[i].id +
+		")'>&#xf00d;</a></td>\
+	  </tr>";
 }
+
+//OS FILTER
+$(document).ready(function () 
+{
+  $(document).on("click", ".os", function () {
+    osSelected = $(this).data("os");
+	displayFilters();
+	displayTable();
+	$("#osButton").html(osSelected);
+  });
+});
+
+//BRAND Filter
+$(document).ready(function () 
+{
+	$(document).on("click", ".brand", function () {
+	  brandSelected = $(this).data("brand");
+	  displayFilters();
+	  displayTable();
+	  $("#brandButton").html(brandSelected);
+	});
+});
+
+//To hide row on selection.
+function hideRow(id) {
+  $("." + id + "." + id).hide();
+}
+
+//To Show row on selection
+function showRow(id) {
+  $("." + id + "." + id).show();
+}
+
+$(document).ready(function(){
+	$("#search").on("keyup", function()
+	{
+		var value = $(this).val().toLowerCase();
+		$("#tableBody tr").filter(function()
+		{
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#filterOS").on("keyup", function()
+	{
+		var value = $(this).val().toLowerCase();
+		$(".os").filter(function()
+		{
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#filterbrand").on("keyup", function()
+	{
+		var value = $(this).val().toLowerCase();
+		$(".brand").filter(function()
+		{
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
